5c76402d1c3e4a199700f2bfcf2b8d7a8f3cf55e
Cedric Ambrosino <cedric.ambrosino.dev@gmail.com>
2021-09-05 13:36:50 +0200

    add hours

--- src/Gestionnaire.java
+++ src/Gestionnaire.java
@@ -1,5 +1,8 @@
 public class Gestionnaire {
     public static void main(String... args){
-        System.out.println("test");
+        TableHoraire planning = new TableHoraire();
+        for(int i=0; i<planning.horaires.length; i++){
+            System.out.println(planning.horaires[i]);
+        }
     }
 }
---
+++ src/TableHoraire.java
@@ -0,0 +1,3 @@
+public class TableHoraire {
+    String[] horaires = {"08h00","09h00","10h00","13h00","17h00","21h00"};
+}
